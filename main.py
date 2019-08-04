0369c58ee66863b9cbe742dbd89cfa4d7523f782
Zeng Fan Pu <zengfanpu@gmail.com>
2019-08-03 20:44:20 -0400

    Update configs

--- main.py
+++ main.py
@@ -6,23 +6,47 @@ import ipdb
 class App():
     def __init__(self):
         self.load_secrets()
-        self.setup_client()
+        self.client = self.setup_client()
 
+    """Loads the access token from .secrets.yaml
+
+    .secrets should NOT be checked into source control
+
+    Sets the following:
+    self.ACCESS_TOKEN
+    """
     def load_secrets(self):
-        with open(".secrets") as file:
+        with open(".secrets.yaml") as file:
             data = file.read()
             secrets = yaml.load(data, Loader=yaml.FullLoader)
-            # self.CLIENT_ID = secrets['CLIENT_ID']
-            # self.CLIENT_SECRET = secrets['CLIENT_ID']
             self.ACCESS_TOKEN =  secrets['ACCESS_TOKEN']
+
+
+    """Loads config from config.yaml
+
+    .secrets should NOT be checked into source control
+
+    Sets the following:
+    self.RENT_COLLECTION_DEADLINE_DAY
+    self.RENT_PAYMENT_DEADLINE_DAY
+    """
+    def load_config(self):
+        with open("config.yaml") as file:
+            data = file.read()
+            config = yaml.load(data, Loader=yaml.FullLoader)
+            self.RENT_COLLECTION_DEADLINE_DAY =  config['RENT_COLLECTION_DEADLINE_DAY']
+            self.RENT_PAYMENT_DEADLINE_DAY = config['RENT_PAYMENT_DEADLINE_DAY']
+
 
     def setup_client(self):
         self.client = asana.Client.access_token(self.ACCESS_TOKEN)
+
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
     logging.info("Asana Household Manager starting...")
     app = App()
+
 
 if __name__ == "__main__":
     main()
